92fef6f57878768040ddf78100730ad48c33f673
Diogo Borges <116685761+diogoborges4@users.noreply.github.com>
2023-09-02 16:04:34 -0300

    Update App.js

--- backend/App.js
+++ backend/App.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use(cors({credentials: true, origin: "http://localhost:3000"}))
+app.use(cors({credentials: true, origin: "https://reactgramapp.netlify.app/"}))
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
